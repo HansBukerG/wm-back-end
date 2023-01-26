3d45a69369035489840a541e1a93ee6a9806b209
Hans Buker <hansbuker1993@gmail.com>
2023-01-26 01:01:40 -0300

    feat: handlers to not Data

--- src/controller/products.controller.go
+++ src/controller/products.controller.go
@@ -10,20 +10,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func GetProductsDefault(writer http.ResponseWriter, request *http.Request){
+func GetProductsDefault(writer http.ResponseWriter, request *http.Request) {
 	var status int
 	products, err := product_service.Read()
 	if err != nil {
 		log.Printf("There is an error in call Read(): " + err.Error())
 		status = http.StatusNotFound
 		products = append(products, utils.EmptyProduct())
-	}else{
+	} else {
 		status = http.StatusAccepted
 	}
 
 	utils.PrintSlice(products)
 	response, _ := json.Marshal(products)
-	executeResponse(writer,status,response)
+	executeResponse(writer, status, response)
 }
 
 func GetProductByString(writer http.ResponseWriter, request *http.Request) {
@@ -35,11 +35,11 @@ func GetProductByString(writer http.ResponseWriter, request *http.Request) {
 	//i call product_service to get my data from MongoDB
 	RegisterFound, err := product_service.SearchByString(searchString)
 	if err != nil {
-		RegisterFound = append(RegisterFound, utils.EmptyProduct())
+		// RegisterFound = append(RegisterFound, utils.EmptyProduct())
 		status = http.StatusNotFound
 	}
 	if len(RegisterFound) == 0 {
-		RegisterFound = append(RegisterFound, utils.EmptyProduct())
+		// RegisterFound = append(RegisterFound, utils.EmptyProduct())
 		status = http.StatusNotFound
 
 	} else {
@@ -49,8 +49,6 @@ func GetProductByString(writer http.ResponseWriter, request *http.Request) {
 	response, _ := json.Marshal(RegisterFound)
 	executeResponse(writer, status, response)
 }
-
-
 
 func executeResponse(writer http.ResponseWriter, status int, response []byte) {
 	writer.Header().Set("Content-Type", "application/json")
